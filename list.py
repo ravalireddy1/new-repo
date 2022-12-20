a719469d1ac22ad13217c76f3fc7d8fb0f86fd1e
ravalireddy1 <121081924+ravalireddy1@users.noreply.github.com>
2022-12-20 23:16:47 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,17 @@
+list=[1,2,3,9,4,5,2,8]
+#insertlist.insert(0,"eddt")
+#print(list)
+#list.append("niyaz")
+#print(list)
+#replacement list[1]="Aslam"
+#print(list)
+#index list[1:]     list[0:4]    list[0:5:2]     list[-1]   list[-4:-1]   list[:-1]
+#print(list)
+#list.remove(2)
+#print(list)
+#list.pop(1)      pop() last item remove
+#print(list)
+#del list[0]
+#print(list)
+new=[x+2 for x in list]
+print(new)
